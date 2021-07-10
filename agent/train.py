d98715312b93844975d454a9efcbfec13877ffca
hy21116 <hy21116@naver.com>
2021-07-11 00:01:45 +0900

    main.py와 train.py 초안

---
+++ agent/train.py
@@ -0,0 +1,43 @@
+import torch
+
+
+def train():
+    train_loader, test_loader = get_data()
+
+    # model
+    model = EfficientDet()
+    model.initialize()
+    model.cuda()
+
+    # Loss, Optimizer, LR scheduler
+    criterion = FocalLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, nestrov=True)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
+
+    # Load Dataset
+    train_loader, test_loader = getData()
+
+    for epoch in range(num_epochs):
+        print('\n')
+        print('Starting epoch {} / {}'.format(epoch, num_epochs))
+
+        # Training.
+        yolo.train()
+        total_loss = 0.0
+        total_batch = 0
+
+        for idx, (x, labels) in enumerate(train_loader):
+            x = x.to('cuda')
+            labels = labels.to('cuda')
+
+            preds = yolo(x)
+            loss = criterion(preds, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # 현재 loss 출력
+            if idx % print_freq == 0:
+                print('Epoch [%d/%d], Iter [%d/%d], LR: %.6f, Loss: %.4f, Average Loss: %.4f'
+                      % (epoch, num_epochs, idx, len(train_loader), lr, loss_this_iter, total_loss / float(total_batch)))
+
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import argparse
+from agent import train
 
+parser = argparse.ArgumentParser(description='YOLO v1')
+parser.add_argument('--train', action='store_true')
+parser.add_argument('--eval', action='store_true')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if args.train:
+        train()

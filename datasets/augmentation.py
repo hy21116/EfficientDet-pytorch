88dd61916ad8a0205f6a6e9d36c5050b4175799c
hy21116 <hy21116@naver.com>
2021-07-13 23:09:58 +0900

    augmentation.py 추가

---
+++ datasets/augmentation.py
@@ -0,0 +1,82 @@
+import cv2
+import torch
+import numpy as np
+from torchvision import transforms
+
+
+class Normalizer(object):
+    def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.mean = np.array([[mean]])
+        self.std = np.array([[std]])
+
+    def __call__(self, sample):
+        image, annots = sample['img'], sample['annot']
+        return {'img': ((image.astype(np.float32) - self.mean) / self.std), 'annot': annots}
+
+
+class Augmenter(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample, flip_x=0.5):
+        if np.random.rand() < flip_x:
+            image, annots = sample['img'], sample['annot']
+            image = image[:, ::-1, :]
+
+            rows, cols, channels = image.shape
+
+            x1 = annots[:, 0].copy()
+            x2 = annots[:, 2].copy()
+
+            x_tmp = x1.copy()
+
+            annots[:, 0] = cols - x2
+            annots[:, 2] = cols - x_tmp
+
+            sample = {'img': image, 'annot': annots}
+        return sample
+
+
+class Resizer(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __init__(self, img_size=512):
+        self.img_size = img_size
+
+    def __call__(self, sample):
+        image, annots = sample['img'], sample['annot']
+        height, width, _ = image.shape
+        if height > width:
+            scale = self.img_size / height
+            resized_height = self.img_size
+            resized_width = int(width * scale)
+        else:
+            scale = self.img_size / width
+            resized_height = int(height * scale)
+            resized_width = self.img_size
+
+        image = cv2.resize(image, (resized_width, resized_height), interpolation=cv2.INTER_LINEAR)
+
+        new_image = np.zeros((self.img_size, self.img_size, 3))
+        new_image[0:resized_height, 0:resized_width] = image
+
+        annots[:, :4] *= scale
+
+        return {'img': torch.from_numpy(new_image).to(torch.float32), 'annot': torch.from_numpy(annots), 'scale': scale}
+
+
+class DefaultTrainTransform(object):
+    def __init__(self, size, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.augment = transforms.Compose([Normalizer(mean=mean, std=std),
+                                           Augmenter(),
+                                           Resizer(size)])
+
+    def __call__(self, sample):
+        return self.augment(sample)
+
+
+class DefaultTestTransform(object):
+    def __init__(self):
+        self.augment = transforms.ToTensor()
+
+    def __call__(self, sample):
+        return self.augment(sample)

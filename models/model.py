ca3719e0860f3999fbaca3c8bf51492d969ed2c1
JeongwooLee <jeongwoolee1124@gmail.com>
2021-07-19 02:49:54 +0900

    Add model.py

---
+++ models/model.py
@@ -0,0 +1,199 @@
+import torch.nn as nn
+import torch
+import math
+from efficientnet_pytorch import EfficientNet
+
+class ConvBlock(nn.Module):
+    def __init__(self, num_channels):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=1, padding=1, groups=num_channels), # depthwise
+            nn.Conv2d(num_channels, num_channels, kernel_size=1, stride=1, padding=0),                      # pointwise
+            nn.BatchNorm2d(num_features=num_channels, momentum=0.9997, eps=4e-5), nn.ReLU())
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class BiFPN_Neck(nn.Module):
+    def __init__(self, num_channels, epsilon=1e-4):
+        super(BiFPN_Neck, self).__init__()
+        self.epsilon = epsilon
+        # Conv layers
+        self.conv6_up = ConvBlock(num_channels)
+        self.conv5_up = ConvBlock(num_channels)
+        self.conv4_up = ConvBlock(num_channels)
+        self.conv3_up = ConvBlock(num_channels)
+        self.conv4_down = ConvBlock(num_channels)
+        self.conv5_down = ConvBlock(num_channels)
+        self.conv6_down = ConvBlock(num_channels)
+        self.conv7_down = ConvBlock(num_channels)
+
+        # Feature scaling layers
+        self.p6_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p5_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p4_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.p3_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+
+        self.p4_downsample = nn.MaxPool2d(kernel_size=2)
+        self.p5_downsample = nn.MaxPool2d(kernel_size=2)
+        self.p6_downsample = nn.MaxPool2d(kernel_size=2)
+        self.p7_downsample = nn.MaxPool2d(kernel_size=2)
+
+        # Weight
+        self.p6_w1 = nn.Parameter(torch.ones(2))
+        self.p6_w1_relu = nn.ReLU()
+        self.p5_w1 = nn.Parameter(torch.ones(2))
+        self.p5_w1_relu = nn.ReLU()
+        self.p4_w1 = nn.Parameter(torch.ones(2))
+        self.p4_w1_relu = nn.ReLU()
+        self.p3_w1 = nn.Parameter(torch.ones(2))
+        self.p3_w1_relu = nn.ReLU()
+
+        self.p4_w2 = nn.Parameter(torch.ones(3))
+        self.p4_w2_relu = nn.ReLU()
+        self.p5_w2 = nn.Parameter(torch.ones(3))
+        self.p5_w2_relu = nn.ReLU()
+        self.p6_w2 = nn.Parameter(torch.ones(3))
+        self.p6_w2_relu = nn.ReLU()
+        self.p7_w2 = nn.Parameter(torch.ones(2))
+        self.p7_w2_relu = nn.ReLU()
+
+    def forward(self, inputs):
+        '''
+        	P7 -------------------------> P7_out -------->
+               |-------------|                ↑
+                             ↓                |
+            P6 ---------> P6_td ---------> P6_out -------->
+               |-------------|--------------↑ ↑
+                             ↓                |
+            P5 ---------> P5_td ---------> P5_out -------->
+               |-------------|--------------↑ ↑
+                             ↓                |
+            P4 ---------> P4_td ---------> P4_out -------->
+               |-------------|--------------↑ ↑
+                             |--------------↓ |
+            P3 -------------------------> P3_out -------->
+        '''
+        # P3, P4, P5, P6 P7
+        p3_in, p4_in, p5_in, p6_in, p7_in = inputs
+
+        # P3_out, P4_out, P5_out, P6_out, P7_out (P7 -> P7_out)
+        # P6, P7 (weight)
+        p6_w1 = self.p6_w1_relu(self.p6_w1)
+        weight = p6_w1 / (torch.sum(p6_w1, dim=0) + self.epsilon)
+        # P6 + P7 -> P6_td (Fast noramlized fusion)
+        p6_up = self.conv6_up(weight[0] * p6_in + weight[1] * self.p6_upsample(p7_in))
+        
+        # P5, P6 (weight)
+        p5_w1 = self.p5_w1_relu(self.p5_w1)
+        weight = p5_w1 / (torch.sum(p5_w1, dim=0) + self.epsilon)
+        # P5 + P6 -> P5_td (Fast noramlized fusion)
+        p5_up = self.conv5_up(weight[0] * p5_in + weight[1] * self.p5_upsample(p6_up))
+
+        # P4, P5 (weight)
+        p4_w1 = self.p4_w1_relu(self.p4_w1)
+        weight = p4_w1 / (torch.sum(p4_w1, dim=0) + self.epsilon)
+        # P4 + P5 -> P4_td (Fast noramlized fusion)
+        p4_up = self.conv4_up(weight[0] * p4_in + weight[1] * self.p4_upsample(p5_up))
+
+        # P3, P4_td (weight)
+        p3_w1 = self.p3_w1_relu(self.p3_w1)
+        weight = p3_w1 / (torch.sum(p3_w1, dim=0) + self.epsilon)
+        # P3 + P4_td -> P3_out (Fast noramlized fusion)
+        p3_out = self.conv3_up(weight[0] * p3_in + weight[1] * self.p3_upsample(p4_up))
+
+        # P4, P4_td, P3_out (weight)
+        p4_w2 = self.p4_w2_relu(self.p4_w2)
+        weight = p4_w2 / (torch.sum(p4_w2, dim=0) + self.epsilon)
+        # P4 + P4_td + P3_out -> P4_out (Fast noramlized fusion)
+        p4_out = self.conv4_down(
+            weight[0] * p4_in + weight[1] * p4_up + weight[2] * self.p4_downsample(p3_out))
+        
+        # P5, P5_td, P4_out (weight)
+        p5_w2 = self.p5_w2_relu(self.p5_w2)
+        weight = p5_w2 / (torch.sum(p5_w2, dim=0) + self.epsilon)
+        # P5 + P5_td + P4_out -> P5_out (Fast noramlized fusion)
+        p5_out = self.conv5_down(
+            weight[0] * p5_in + weight[1] * p5_up + weight[2] * self.p5_downsample(p4_out))
+        
+        # P6, P6_td, P5_out (weight)
+        p6_w2 = self.p6_w2_relu(self.p6_w2)
+        weight = p6_w2 / (torch.sum(p6_w2, dim=0) + self.epsilon)
+        # P6 + P6_td + P5_out -> P6_out (Fast noramlized fusion)
+        p6_out = self.conv6_down(
+            weight[0] * p6_in + weight[1] * p6_up + weight[2] * self.p6_downsample(p5_out))
+        
+        # P7, P6_out (weight)
+        p7_w2 = self.p7_w2_relu(self.p7_w2)
+        weight = p7_w2 / (torch.sum(p7_w2, dim=0) + self.epsilon)
+        # P7 + P6_out -> P7_out (Fast noramlized fusion)
+        p7_out = self.conv7_down(weight[0] * p7_in + weight[1] * self.p7_downsample(p6_out))
+
+        return p3_out, p4_out, p5_out, p6_out, p7_out
+
+
+class Regressor_Head(nn.Module):
+    def __init__(self, in_channels, num_anchors, num_layers):
+        super(Regressor_Head, self).__init__()
+        layers = []
+        for _ in range(num_layers):
+            layers.append(nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.ReLU(True))
+        self.layers = nn.Sequential(*layers)
+        self.header = nn.Conv2d(in_channels, num_anchors * 4, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, inputs):
+        inputs = self.layers(inputs)
+        inputs = self.header(inputs)
+        output = inputs.permute(0, 2, 3, 1)
+        return output.contiguous().view(output.shape[0], -1, 4)
+        # contiguous : 새로운 메모리 공간에 데이터를 복사하여 주소값 연속성을 가변적으로 만들어줌
+
+class Classifier_Head(nn.Module):
+    def __init__(self, in_channels, num_anchors, num_classes, num_layers):
+        super(Classifier_Head, self).__init__()
+        self.num_anchors = num_anchors
+        self.num_classes = num_classes
+        layers = []
+        for _ in range(num_layers):
+            layers.append(nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.ReLU(True))
+        self.layers = nn.Sequential(*layers)
+        self.header = nn.Conv2d(in_channels, num_anchors * num_classes, kernel_size=3, stride=1, padding=1)
+        self.act = nn.Sigmoid()
+
+    def forward(self, inputs):
+        inputs = self.layers(inputs)
+        inputs = self.header(inputs)
+        inputs = self.act(inputs)
+        inputs = inputs.permute(0, 2, 3, 1)
+        output = inputs.contiguous().view(inputs.shape[0], inputs.shape[1], inputs.shape[2], self.num_anchors,
+                                          self.num_classes)
+
+        return output.contiguous().view(output.shape[0], -1, self.num_classes)
+
+
+class EfficientNet_Backbone(nn.Module):
+    def __init__(self, ):
+        super(EfficientNet_Backbone, self).__init__()
+        model = EfficientNet.from_pretrained('efficientnet-b0')
+        del model._conv_head
+        del model._bn1
+        del model._avg_pooling
+        del model._dropout
+        del model._fc
+        self.model = model
+
+    def forward(self, x):
+        x = self.model._swish(self.model._bn0(self.model._conv_stem(x)))
+        feature_maps = []
+        for idx, block in enumerate(self.model._blocks):
+            drop_connect_rate = self.model._global_params.drop_connect_rate
+            if drop_connect_rate:
+                drop_connect_rate *= float(idx) / len(self.model._blocks)
+            x = block(x, drop_connect_rate=drop_connect_rate)
+            if block._depthwise_conv.stride == [2, 2]:
+                feature_maps.append(x)
+
+        return feature_maps[1:]

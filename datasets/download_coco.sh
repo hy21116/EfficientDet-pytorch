0de5638b3bab5ed3042917a17309de0d9fd2d5b7
JeongwooLee <jeongwoolee1124@gmail.com>
2021-07-27 13:11:41 +0900

    annotations_trainval2017에 zip 파일 확장명 추가

--- datasets/download_coco.sh
+++ datasets/download_coco.sh
@@ -9,10 +9,10 @@ wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip train2017.zip
 unzip val2017.zip
 unzip test2017.zip
-unzip annotations_trainval2017
+unzip annotations_trainval2017.zip
 mv annotations_trainval2017 annotations
 
 rm train2017.zip
 rm val2017.zip
 rm test2017.zip
-rm annotations_trainval2017
+rm annotations_trainval2017.zip

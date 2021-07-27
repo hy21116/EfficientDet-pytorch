8ca07ed8f159e358c4b6bcd8da4bcf17b2fc5a01
JeongwooLee <jeongwoolee1124@gmail.com>
2021-07-28 08:48:17 +0900

    loss.py에 반환값 추가

--- agent/loss.py
+++ agent/loss.py
@@ -168,3 +168,5 @@ class FocalLoss(nn.Module):
                 else:
                     regression_losses.append(torch.tensor(0).to(dtype))
 
+            return torch.stack(classification_losses).mean(dim=0, keepdim=True), \
+               torch.stack(regression_losses).mean(dim=0, keepdim=True) * 50

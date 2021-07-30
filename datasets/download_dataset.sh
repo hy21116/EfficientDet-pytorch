252c20bb3e1687940107977439d83d8a6734010f
JeongwooLee <jeongwoolee1124@gmail.com>
2021-07-31 01:46:15 +0900

    download voc dataset 추가

--- datasets/download_dataset.sh
+++ datasets/download_dataset.sh
@@ -1,18 +1,18 @@
 start=`date +%s`
 
 # Download VOC2007 DATASET
-echo "Downloading VOC2007 dataset ..."
+echo "Downloading VOC2007 dataset ... "
 wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar 
 wget http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 
 # Extract tar files
-echo "Extracting trainval ..."
+echo "Extracting trainval ... "
 tar -xf train/VOCtrainval_06-Nov-2007.tar 
-echo "Extracting test ..."
+echo "Extracting test ... "
 tar -xf test/VOCtest_06-Nov-2007.tar 
 
 # Remove tar files
-echo "removing tars ..."
+echo "removing tars ... "
 rm -rf test/VOCtest_06-Nov-2007.tar
 rm -rf train/VOCtrainval_06-Nov-2007.tar
 
@@ -20,3 +20,4 @@ end=`date +%s`
 runtime=$((end-start))
 
 echo "Completed in" $runtime "seconds"
+ㅎ
